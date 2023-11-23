848e1c1f16955cc91bae3d498d404788fb90c465
ccwav <ccwav@163.com>
2023-11-23 14:13:07 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+class Program
+{
+    private static string oldIP = "";
+    private static string strBearer = "";
+
+    public class Config
+    {
+        public string Bearer { get; set; }
+    }
+
+    static async Task Main()
+    {
+        var json = File.ReadAllText("LuncherConfig.json");
+        var config = JsonConvert.DeserializeObject<Config>(json);
+        strBearer = config.Bearer;
+        using (HttpClient client = new HttpClient())
+        {
+            Timer timer = new Timer(CheckIP, null, 0, 60000);
+            await Task.Run(() => Console.ReadLine());
+        }
+    }
+
+    static async void CheckIP(object? state)
+    {
+        string newIP = await GetPublicIP();
+        if (oldIP != newIP)
+        {
+            oldIP = newIP;
+            Console.WriteLine($"IP changed to {newIP}. Downloading file...");
+            await DownloadFile();
+            RestartProcess("PandoraNext.exe");
+        }
+    }
+
+    static async Task<string> GetPublicIP()
+    {
+        string url = "https://myip.ipip.net";
+        using (HttpClient client = new HttpClient())
+        {
+            HttpResponseMessage response = await client.GetAsync(url);
+            string data = await response.Content.ReadAsStringAsync();
+            Regex regex = new Regex(@"(\d+\.\d+\.\d+\.\d+)");
+            Match match = regex.Match(data);
+            return match.Success ? match.Value : string.Empty;
+        }
+    }
+
+    static async Task DownloadFile()
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", strBearer);
+            var response = await client.GetAsync("https://dash.pandoranext.com/data/license.jwt");
+            var content = await response.Content.ReadAsByteArrayAsync();
+            await File.WriteAllBytesAsync("license.jwt", content);
+        }
+    }
+
+    static void RestartProcess(string processName)
+    {
+        foreach (var process in System.Diagnostics.Process.GetProcessesByName("PandoraNext"))
+        {
+            process.Kill();
+            process.WaitForExit();
+        }
+        System.Diagnostics.Process.Start(processName);
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# PandoraNext_Luncher
